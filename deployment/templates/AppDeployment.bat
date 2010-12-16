52e46a8e8706072f9411fbd5f2729aede78f18ed
alexander.beletsky <alexander.beletsky@gmail.com>
2010-12-16 19:39:38 +0200

    corrected build scripts to remove unit testing files and redudant configs;

--- deployment/templates/AppDeployment.bat
+++ deployment/templates/AppDeployment.bat
@@ -4,6 +4,12 @@ SET DIR=%~d0%~p0%
 
 SET web.deploy.folder="${web.deploy.folder}"
 
+echo copy application content
 rmdir /s /q %web.deploy.folder%
 xcopy /E /F /H /R ..\_PublishedWebSites\Web %web.deploy.folder%
 xcopy ..\build_artifacts\_BuildInfo.xml %web.deploy.folder%
+
+echo remove redudant files
+del %web.deploy.folder%*Tests*.html
+del %web.deploy.folder%Web.Debug.config  
+del %web.deploy.folder%Web.Release.config 
