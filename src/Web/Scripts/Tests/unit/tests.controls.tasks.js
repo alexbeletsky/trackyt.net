6fc6f6b65650c88ab748200b5314061fc8bdeef4
alexander.beletsky <alexander.beletsky@gmail.com>
2010-12-27 22:27:20 +0200

    new tasks control with respect to tdd started;

---
+++ src/Web/Scripts/Tests/unit/tests.controls.tasks.js
@@ -0,0 +1,98 @@
+﻿$(function () {
+    module("tasks control tests",
+        {
+            setup: function () {
+                $("#hidden_test_dom").append($('<div id="tasks"/>'));
+            },
+            teardown: function () {
+                $("#hidden_test_dom").empty();
+            }
+        }
+        );
+
+    test("smoke test", function () {
+
+        var control = new tasksControl();
+        ok(control != null);
+    });
+
+    test("add new task to control", function () {
+
+        // assert
+        var control = new tasksControl($('#tasks'));
+        var task = { id: 0, description: "task 1", status: 0, createdDate: null, startedDate: null, stoppedDate: null };
+
+        // act
+        control.addTask(task);
+
+        // post
+        var currentTasks = control.tasksCount();
+        ok(currentTasks == 1, "task has not been added to control");
+    });
+
+    test("add new task add tasks ui", function () {
+
+        // assert
+        var control = new tasksControl($('#tasks'));
+        var task = { id: 0, description: "task 1", status: 0, createdDate: null, startedDate: null, stoppedDate: null };
+
+        // act
+        control.addTask(task);
+
+        // post
+        var currentTasks = $('#tasks .task').size();
+        ok(currentTasks == 1, "task has not been added to control");
+    });
+
+//    test("add new task calls callback", function () {
+//        var me = this;
+
+//        // assert
+//        var control = new tasksControl($('#tasks'));
+//        var task = { id: 0, description: "task 1", status: 0, createdDate: null, startedDate: null, stoppedDate: null };
+//        this.handlerCalled = false;
+
+//        function onTaskAddedHanlder(task) {
+//            me.handlerCalled = true;
+//        }
+
+//        // act
+//        control.onTaskAdded(onTaskAddedHanlder);
+
+//        // assert
+//        control.addTask(task);
+//        ok(this.handlerCalled, "onTaskAdded has not been called");
+//    });
+
+    test("remove task from control", function () {
+        // assert
+        var control = new tasksControl($('#tasks'));
+        var task = { id: 0, description: "task 1", status: 0, createdDate: null, startedDate: null, stoppedDate: null };
+        control.addTask(task);
+
+        // act
+        control.removeTask(task.id);
+
+        // assert
+        var currentTasks = control.tasksCount();
+        ok(currentTasks == 0, "task has not been removed from control");
+    });
+
+    //    test("control calls getAllTasks and create tasks", function () {
+
+    //        function getAllTasks(callback) {
+    //            var tasks = [
+    //                    { id: 0, description: "task 1", status: 0, createdDate: null, startedDate: null, stoppedDate: null },
+    //                    { id: 1, description: "task 2", status: 0, createdDate: null, startedDate: null, stoppedDate: null }
+    //                ];
+
+    //            callback(tasks);
+    //        }
+
+    //        var control = tasksControl($('#tasks'), getAllTasks);
+
+    //        var tasks = $('#tasks .task').size();
+    //        ok(tasks == 2, "2 tasks added");
+    //    });
+
+});
---
+++ src/Web/Scripts/Tracky/controls.tasks.js
@@ -0,0 +1,68 @@
+﻿function tasksControl(div) {
+    this.div = div;
+    this.tasks = [];
+}
+
+tasksControl.prototype = (function () {
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // private members
+
+    var onTaskAddedHandler = null;
+
+    function task(control, t) {
+        this.id = t.id;
+        this.control = control;
+        this.control.div.append('<div class="task"></div>');
+    }
+
+    task.prototype.remove = function() {
+
+    }
+
+
+    return {
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////
+        // public members
+
+        addTask: function (t) {
+            var taskToAdd = new task(this, t);
+            this.tasks.push(taskToAdd);
+
+//            if (onTaskAddedHandler) {
+//                onTaskAddedHandler(taskToAdd);
+//            }
+        },
+
+        removeTask: function (id) {
+            
+            for(var t in this.tasks) {
+                if (this.tasks[t].id == id) {
+                    // remove from array
+                    var tasksToRemove = this.tasks[t];
+                    this.tasks = $.grep(this.tasks, function(val) { val != tasksToRemove; });
+                    // call remove handler of task
+                    tasksToRemove.remove();
+                }
+            }
+
+        },
+
+        tasksCount: function () {
+            return this.tasks.length;
+        },
+
+//        onTaskAdded: function (h) {
+//            onTaskAddedHandler = h;
+//        },
+
+    };
+})();
+
+function createTasksControl() {
+
+    var control = new tasksControl(div);
+
+    return control;
+} 
