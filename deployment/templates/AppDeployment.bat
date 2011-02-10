a1ad1a3ebac9034c5089fe766f55711d270c6075
alexander.beletsky <alexander.beletsky@gmail.com>
2011-02-10 20:21:40 +0200

    configuration correction

--- deployment/templates/AppDeployment.bat
+++ deployment/templates/AppDeployment.bat
@@ -10,6 +10,6 @@ xcopy /E /F /H /R ..\_PublishedWebSites\Web %web.deploy.folder%
 xcopy ..\build_artifacts\_BuildInfo.xml %web.deploy.folder%
 
 echo remove redudant files
-del %web.deploy.folder%*Tests*.html
+del %web.deploy.folder%*Tests*.htm*
 del %web.deploy.folder%Web.Debug.config  
 del %web.deploy.folder%Web.Release.config 
