bbd2934b0a93bd2061b7d47b42cd0d6ed0e258ae
alexander.beletsky <alexander.beletsky@gmail.com>
2011-05-13 23:41:21 +0300

    Added new desing and fix for logo;

---
+++ design/dashboard4/Scripts/jquery.confirm.js
@@ -0,0 +1,43 @@
+﻿$().ready(function () {
+
+    $.confirm = function (params) {
+        var buttonsMarkup = '';
+        $.each(params.buttons, function (name, obj) {
+            buttonsMarkup += '<a href="#" class="button">' + name + '<span></span></a>';
+            if (!obj.action) {
+                obj.action = function () { };
+            }
+        });
+
+        var markup = [
+            '<div id="confirmOverlay">',
+            '<div id="confirmBox">',
+            '<p>', params.message, '</p>',
+            '<div id="confirmButtons">',
+            buttonsMarkup,
+            '</div></div></div>'
+        ].join('');
+
+        $(markup).hide().appendTo('body').fadeIn();
+
+        $('#confirmBox .button').each(function (name, button) {
+            $(button).click(function () {
+                $.confirm.hide(function () {
+                    params.buttons[$(button).text()].action();
+                });
+                return false;
+            });
+        });
+    };
+
+    $.confirm.hide = function (callback) {
+        $('#confirmOverlay').fadeOut(function () {
+            $(this).remove();
+
+            if (callback) {
+                callback();
+            }
+        });
+    };
+});
+        
